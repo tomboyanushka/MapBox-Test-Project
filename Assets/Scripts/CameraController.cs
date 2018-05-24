7a2935ebf352847bf4be6926dc3e419e7879b323
tomboyanushka <32500051+tomboyanushka@users.noreply.github.com>
2018-05-24 16:20:47 -0400

    More mapbox tests

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,43 +4,25 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public Transform target;
-    Camera cam;
-    public Vector3 offset = new Vector3(0, 5, -8);
-    public float XTilt = 10;
-    public float smooth = 0.09f;
-
-    Vector3 destination = Vector3.zero;
-    PlayerController charController;
-    float rotateVelocity = 0;
-
-
-
+    public GameObject target;
+    public float damping = 1;
+    Vector3 offset;
 
     void Start()
     {
-        charController = target.GetComponent<PlayerController>();
+        offset = target.transform.position - transform.position;
     }
 
     void LateUpdate()
     {
+        float currentAngle = transform.eulerAngles.y;
+        float desiredAngle = target.transform.eulerAngles.y;
+        float angle = Mathf.LerpAngle(currentAngle, desiredAngle, Time.deltaTime * damping);
 
-        MoveToTarget();
-        LookAtTarget();
+        Quaternion rotation = Quaternion.Euler(0, angle, 0);
+        transform.position = target.transform.position - (rotation * offset);
+
+        transform.LookAt(target.transform);
 
     }
-
-
-    void MoveToTarget()
-    {
-        destination = charController.PlayerRotation * offset;
-        destination += target.position;
-        transform.position = destination;
-    }
-    void LookAtTarget()
-    {
-        float eulerAngleY = Mathf.SmoothDampAngle(transform.eulerAngles.y, target.eulerAngles.y, ref rotateVelocity, smooth);
-        transform.rotation = Quaternion.Euler(transform.eulerAngles.x, eulerAngleY, 0);
-    }
-
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,165 +1,25 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
 
-    public float inputDelay = 0.1f;
-    public float forwardVelocity = 12;
-    public float rotateVelocity = 100;
-    public float jumpVelocity = 25;
-    public float distToGround = 0.1f;
-    public LayerMask Ground;
-    public float downwardAccel = 1.0f;
-    private float toolCount = 0;
-    private bool isDead = false;
+    public float speed;
 
-    Animator anim;
+    private Rigidbody rb;
 
-    Vector3 velocity = Vector3.zero;
-
-    bool isGrounded()
-    {
-        return Physics.Raycast(transform.position, Vector3.down, distToGround, Ground);
-    }
-
-
-    public float health = 100f;
-
-    Quaternion playerRotation;
-
-    Rigidbody rigidbody;
-    float forwardInput;
-    float rotateInput;
-    float jumpInput;
-
-    public Quaternion PlayerRotation
-    {
-        get { return playerRotation; }
-    }
-
-    // Use this for initialization
     void Start()
     {
-        playerRotation = transform.rotation;
-        if (GetComponent<Rigidbody>())
-        {
-            rigidbody = GetComponent<Rigidbody>();
-
-        }
-        else Debug.LogError("Character needs a rigidbody");
-
-        forwardInput = rotateInput = jumpInput = 0;
-
-        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
     }
 
-    void getInput()
+    void FixedUpdate()
     {
-        forwardInput = Input.GetAxis("Vertical");
-        rotateInput = Input.GetAxis("Horizontal");
-        jumpInput = Input.GetAxisRaw("Jump");
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
     }
-
-    private void Update()
-    {
-        getInput();
-        Rotate();
-
-        if (toolCount == 5 && isDead == false)
-        {
-            PlayerWins();
-        }
-
-        if (health == 0)
-        {
-            isDead = true;
-            PlayerDies();
-        }
-    }
-
-    private void FixedUpdate()
-    {
-        Move();
-        Jump();
-
-        rigidbody.velocity = transform.TransformDirection(velocity);
-    }
-
-    private void Move()
-    {
-        if (Mathf.Abs(forwardInput) > inputDelay)
-        {
-            velocity.z = forwardInput * forwardVelocity;
-        }
-        else
-            velocity.z = 0;
-        anim.Play("Robo2 Idle");
-    }
-
-    private void Jump()
-    {
-        if (jumpInput > 0 && isGrounded())
-        {
-            velocity.y = jumpVelocity;
-        }
-        else if (jumpInput == 0 && isGrounded())
-        {
-            velocity.y = 0;
-        }
-        else
-        {
-            velocity.y -= downwardAccel;
-        }
-
-    }
-
-    private void Rotate()
-    {
-        if (Mathf.Abs(rotateInput) > inputDelay)
-        {
-            playerRotation *= Quaternion.AngleAxis(rotateVelocity * rotateInput * Time.deltaTime, Vector3.up);
-            transform.rotation = playerRotation;
-
-        }
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag == "minute")
-        {
-            health -= 10f;
-            Debug.Log(health);
-        }
-        else if (collision.gameObject.tag == "hour")
-        {
-            health -= 20f;
-            Debug.Log(health);
-
-        }
-        else if (collision.gameObject.tag == "Stand")
-        {
-            health = 100f;
-            Debug.Log(health);
-
-        }
-        else if (collision.gameObject.tag == "Tool")
-        {
-            anim.Play("");
-            Destroy(collision.gameObject);
-            toolCount += 1;
-        }
-    }
-
-    void PlayerWins()
-    {
-        Debug.Log("You Win!");
-    }
-
-    void PlayerDies()
-    {
-        Debug.Log("Game Over");
-    }
-
 }
